767d3eacd26315baae4dce00bf366d604f926dd3
yangguowei <yangguowei@kuaizi.co>
2019-09-30 18:06:41 +0800

    化写法你

--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,18 +1,20 @@
 import actionTypes from '../actionTypes'
+
 const initState = {
   userInfo: JSON.parse(window.localStorage.getItem('userInfo') || '{}'),
   isLoading: 0
 }
 
 const ACTION_HANDLERS = {
-  [actionTypes.START_LOGIN]: (state, action) => {
+  [actionTypes.START_LOGIN](state, action) {
+    console.log('action', action)
     return {
       ...state,
       userInfo: action.userInfo,
       isLoading: 1
     }
   },
-  [actionTypes.LOGIN_FAILED]: (state, action) => {
+  [actionTypes.LOGIN_FAILED](state, action) {
     return {
       ...state,
       userInfo: {},
